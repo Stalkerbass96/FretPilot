dd4b91ba12e5ac4eba2c7b0571525b745fb1499b
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-13 15:50:36 +0900

    feat: expose articulation planner API

---
+++ src/fretpilot/articulation/__init__.py
@@ -0,0 +1,6 @@
+"""Generic guitar articulation planning."""
+
+from fretpilot.articulation.models import ArticulationPlan
+from fretpilot.articulation.planner import plan_articulations
+
+__all__ = ["ArticulationPlan", "plan_articulations"]
