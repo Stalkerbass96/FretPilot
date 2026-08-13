c0decd3310e254824fca225f3afd60f24174535f
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-13 17:39:01 +0900

    feat: expose virtual guitar instrument profile models

---
+++ src/fretpilot/virtual_instruments/__init__.py
@@ -0,0 +1,15 @@
+"""Virtual-guitar adapter knowledge and capability models."""
+
+from fretpilot.virtual_instruments.models import (
+    AdapterEvidence,
+    ArticulationCapability,
+    ControlAction,
+    VirtualGuitarInstrumentProfile,
+)
+
+__all__ = [
+    "AdapterEvidence",
+    "ArticulationCapability",
+    "ControlAction",
+    "VirtualGuitarInstrumentProfile",
+]
