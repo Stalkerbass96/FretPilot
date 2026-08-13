4903615937fbd1155d1286a722532cdbf8206d0d
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-13 16:08:37 +0900

    feat: expose guitar behavior knowledge library

---
+++ src/fretpilot/knowledge/__init__.py
@@ -0,0 +1,9 @@
+"""Versioned musical knowledge used by FretPilot classifiers."""
+
+from fretpilot.knowledge.guitar_behaviors import (
+    LIBRARY_VERSION,
+    PROFILES,
+    match_behavior_profiles,
+)
+
+__all__ = ["LIBRARY_VERSION", "PROFILES", "match_behavior_profiles"]
