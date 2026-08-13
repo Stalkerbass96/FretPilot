9ed1c94f60b705a3286ca19d676f08709d7d9fba
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-13 18:03:01 +0900

    feat: export section segmentation API

--- src/fretpilot/analysis/__init__.py
+++ src/fretpilot/analysis/__init__.py
@@ -1,5 +1,16 @@
 """High-level musical analysis pipelines."""
 
 from fretpilot.analysis.guitar import GuitarTrackAnalysis, analyze_guitar_track
+from fretpilot.analysis.sections import (
+    GuitarSection,
+    SectionSegmentation,
+    segment_instrument_stream,
+)
 
-__all__ = ["GuitarTrackAnalysis", "analyze_guitar_track"]
+__all__ = [
+    "GuitarSection",
+    "GuitarTrackAnalysis",
+    "SectionSegmentation",
+    "analyze_guitar_track",
+    "segment_instrument_stream",
+]
