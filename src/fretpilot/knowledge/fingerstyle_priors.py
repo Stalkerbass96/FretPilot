70e38fb1fa17bf88a29259bd81b3f99e4fd5d096
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-14 02:16:02 +0900

    feat: add fingerstyle guitar priors

---
+++ src/fretpilot/knowledge/fingerstyle_priors.py
@@ -0,0 +1,11 @@
+"""Fingerstyle guitar priors distilled from cited educational sources."""
+
+FINGERSTYLE_PRIORS = {
+    "alternating_bass_thumb": 1.70,
+    "bass_treble_voice_separation": 1.55,
+    "open_string_usage": 1.30,
+    "let_ring": 1.40,
+    "note_overlap": 1.25,
+    "syncopation_preservation": 1.35,
+    "source_ids": ("fender-travis-picking",),
+}
