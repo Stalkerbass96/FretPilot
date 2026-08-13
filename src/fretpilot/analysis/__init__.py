860e7c9019c8e6429510d9b6f3ae91a9e288ba8b
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-13 15:52:57 +0900

    feat: expose guitar analysis pipeline

---
+++ src/fretpilot/analysis/__init__.py
@@ -0,0 +1,5 @@
+"""High-level musical analysis pipelines."""
+
+from fretpilot.analysis.guitar import GuitarTrackAnalysis, analyze_guitar_track
+
+__all__ = ["GuitarTrackAnalysis", "analyze_guitar_track"]
