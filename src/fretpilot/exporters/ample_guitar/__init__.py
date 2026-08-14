82ea45611068b6860cbe00fba4c3e3afe0616995
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-14 13:34:48 +0900

    feat: make public Ample export use generic VI profile handoff

--- src/fretpilot/exporters/ample_guitar/__init__.py
+++ src/fretpilot/exporters/ample_guitar/__init__.py
@@ -1,13 +1,13 @@
 """Ample Guitar performance-MIDI adapters."""
 
+from fretpilot.exporters.ample_guitar.generic_renderer import (
+    AmpleExportResult,
+    export_ample_sc_midi,
+)
 from fretpilot.exporters.ample_guitar.profiles import (
     AMPLE_GUITAR_SC_V4,
     AmpleGuitarProfile,
     get_profile,
-)
-from fretpilot.exporters.ample_guitar.renderer import (
-    AmpleExportResult,
-    export_ample_sc_midi,
 )
 
 __all__ = [
