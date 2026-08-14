75f5a12421f3058ae7fbc513cf8a16aeda0bf0fe
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-14 13:18:41 +0900

    feat: expose migrated Ample generic VI profile

--- src/fretpilot/virtual_instruments/__init__.py
+++ src/fretpilot/virtual_instruments/__init__.py
@@ -1,5 +1,6 @@
 """Virtual-guitar adapter knowledge and capability models."""
 
+from fretpilot.virtual_instruments.ample_guitar_sc import AMPLE_GUITAR_SC_V4_PROFILE
 from fretpilot.virtual_instruments.models import (
     AdapterEvidence,
     ArticulationCapability,
@@ -8,6 +9,7 @@ from fretpilot.virtual_instruments.models import (
 )
 
 __all__ = [
+    "AMPLE_GUITAR_SC_V4_PROFILE",
     "AdapterEvidence",
     "ArticulationCapability",
     "ControlAction",
