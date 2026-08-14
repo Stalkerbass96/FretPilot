c9391676413b3e7f9f02a76a2101456d85d0002b
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-14 13:24:46 +0900

    feat: expose VI capability reports

--- src/fretpilot/virtual_instruments/__init__.py
+++ src/fretpilot/virtual_instruments/__init__.py
@@ -1,6 +1,11 @@
 """Virtual-guitar adapter knowledge and capability models."""
 
 from fretpilot.virtual_instruments.ample_guitar_sc import AMPLE_GUITAR_SC_V4_PROFILE
+from fretpilot.virtual_instruments.capability_report import (
+    CapabilityReport,
+    CapabilityRequirement,
+    build_capability_report,
+)
 from fretpilot.virtual_instruments.models import (
     AdapterEvidence,
     ArticulationCapability,
@@ -18,9 +23,12 @@ __all__ = [
     "AMPLE_GUITAR_SC_V4_PROFILE",
     "AdapterEvidence",
     "ArticulationCapability",
+    "CapabilityReport",
+    "CapabilityRequirement",
     "CapabilityResolution",
     "ControlAction",
     "VirtualGuitarInstrumentProfile",
+    "build_capability_report",
     "get_profile",
     "list_profiles",
     "negotiate_intent",
