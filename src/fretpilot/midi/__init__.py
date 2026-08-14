581d3890119bd207d8910b6dd7c54eb5c5d3d094
Stalkerbass96 <stalkerbass96@gmail.com>
2026-08-14 09:46:47 +0900

    feat: expose pitch-wheel enriched MIDI loader

--- src/fretpilot/midi/__init__.py
+++ src/fretpilot/midi/__init__.py
@@ -1,6 +1,6 @@
 """MIDI import layer."""
 
 from fretpilot.midi.models import NormalizedTimeline
-from fretpilot.midi.parser import load_midi
+from fretpilot.midi.pitch_wheel import load_midi
 
 __all__ = ["NormalizedTimeline", "load_midi"]
